d9b541b019b685e0ee1d3e3a01dfca72da688f49
OmarGmzRmz <gomar8138@gmail.com>
2021-04-13 17:37:01 -0500

    Added function to send email using sendgrid

---
+++ sendFeedbackSendGrid.js
@@ -0,0 +1,35 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(''); // Add your SendGrid API key
+
+exports.handler = async (event) => {
+    const msg = {
+        from: 'Omar Eliseo Gómez Raírez <gomar8138@gmail.com>',
+        to: ['gomar8138@gmail.com'],
+        subject: 'Personal Website Feedback',
+        html : `
+            <html>
+                <head></head>
+                <body>
+                    <p>Got feedback from personal website visitor.</p>
+                    <h3>Feedback</h3>
+                    <p>${event.description}</p>
+                    <img src="${event.screenshot}" alt="screenshot of the issue">
+                    <p style="font-size: 10px color: dimgray">Desable image blocking in your email client to properly visualize this message.</p>
+                </body>
+            </html>
+        `
+    }; try {
+        await sgMail.send(msg);
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Feedback email sent!'),
+        };
+    } catch (error) {
+        console.log(error);
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify('Could not send feedback email.')
+        };
+    }
+    return response;
+};
